297ecf888eccefdd1d6fc48490adfee4569e0c56
Omar Qurashi <96021536+OmarQurashi868@users.noreply.github.com>
2024-03-10 15:59:41 +0400

    Used db classes in utils

--- utils/userAuth.js
+++ utils/userAuth.js
@@ -1,34 +1,25 @@
-const knex = require('knex')({
-    client: 'sqlite3',
-    connection: {
-        filename: './database.db',
-    },
-    useNullAsDefault: true,
-});
+const Token = require("../classes/token");
 
 const verifyToken = async (req, res, next) => {
-    await purgeExpiredTokens();
+    await Token.purgeExpiredTokens();
 
-    const tokenMissing = req.body['token'] == null;
+    const tokenMissing = req.body['token'] == undefined;
     if (tokenMissing) {
-        res.status(403).send({ message: "Token is missing" });
+        res.status(403);
+        res.send({ message: "Token is missing" });
         return;
     }
 
     const token = req.body['token']
-    const tokenData = knex.select().table("tokens").where({ token }).first();
-    const tokenInvalid = tokenData == null
+    const tokenData = Token.find(token);
+    const tokenInvalid = tokenData == undefined
     if (tokenInvalid) {
-        res.status(403).send({ message: "Token is invalid" });
+        res.status(403);
+        res.send({ message: "Token is invalid" });
         return;
     }
 
     next();
-}
-
-const purgeExpiredTokens = async () => {
-    const currentTime = new Date();
-    knex("tokens").where('expiryDate', '<', currentTime).delete();
 }
 
 module.exports = verifyToken;
